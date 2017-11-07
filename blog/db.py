c830a6e63198248d863056b904adb345236d7f12
John Nolette <john@neetgroup.net>
2017-11-07 00:21:12 -0500

    Added user mongo document

--- blog/db.py
+++ blog/db.py
@@ -1,2 +1,13 @@
 from mongoengine import *
 
+
+class User(Document):
+
+    username = StringField()
+    full_name = StringField()
+    posts = fields.ListField(fields.StringField)
+    total_likes = IntField()
+    last_posted = DateTimeField()
+    last_activity = DateTimeField()
+
+
