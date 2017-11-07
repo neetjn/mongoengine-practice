71c8a7f246958970b7da5f11a48fa7092401eb4d
John Nolette <john@neetgroup.net>
2017-11-07 00:07:47 -0500

    Finished comment serializer

--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,49 @@
-"""
-sdfs
-"""
-
 from r2dto import fields, validators, Serializer
+
+
+class Profile
+
+
+class Comment(object):
+
+    def __init__(self, **kwargs):
+
+        self.author = kwargs.get('author', '')
+        self.content = kwargs.get('content', '')
+        self.tags = kwargs.get('tags', [])
+        self.created = kwargs.get('created', None)
+        self.edited = kwargs.get('edited', None)
+        self.comments = kwargs.get('comments', [])
+        self.likes = kwargs.get('likes', 0)
+
+
+class CommentSerializer(Serializer):
+    
+    author = fields.StringField()
+    content = fields.StringField()
+    created = fields.DateTimeField()
+    edited = fields.DateTimeField()
+    likes = fields.IntegerField()
+    views = fields.IntegerField()
+
+    class Meta(object):
+
+        model = Comment
 
 
 class Post(object):
 
-    def __init__(self, title='', author='', content='', created=None, edited=None, likes=0, views=0):
-        self.title = title
-        self.author = author
-        self.content = content
-        self.created = created
-        self.edited = edited
-        self.views = views
+    def __init__(self, **kwargs):
+
+        self.title = kwargs.get('title', '')
+        self.author = kwargs.get('author', '')
+        self.content = kwargs.get('content', '')
+        self.tags = kwargs.get('tags', [])
+        self.created = kwargs.get('created', None)
+        self.edited = kwargs.get('edit', None)
+        self.comments = kwargs.get('comments', [])
+        self.likes = kwargs.get('likes', 0)
+        self.views = kwargs.get('views', 0)
 
 
 class PostSerializer(Serializer):
@@ -23,8 +53,10 @@ class PostSerializer(Serializer):
     content = fields.StringField()
     created = fields.DateTimeField()
     edited = fields.DateTimeField()
+    comments = fields.ListField(CommentSerializer)
     likes = fields.IntegerField()
     views = fields.IntegerField()
 
-    class Meta:
+    class Meta(object):
+
         model = Post
