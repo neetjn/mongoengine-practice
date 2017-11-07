47372c777ad1fba215573256676f81eb65ac65ec
John Nolette <john@neetgroup.net>
2017-11-07 00:16:49 -0500

    Finished user model and serializer

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,33 @@
 from r2dto import fields, validators, Serializer
 
 
-class Profile
+class User(object):
+
+    def __init__(self, **kwargs):
+
+        self.username = kwargs.get('username', '')
+        self.full_name = kwargs.get('full_name', '')
+        self.posts = kwargs.get('posts', [])
+        self.total_likes = kwargs.get('total_links', 0)
+        self.last_posted = kwargs.get('last_posted', None)
+        self.last_activity = kwargs.get('last_activity', None)
+
+
+class UserSerializer(object):
+
+    username = fields.StringField()
+    full_name = fields.StringField(name='fullName')
+    posts = fields.ListField(fields.StringField)
+    total_likes = fields.IntegerField(name='totalLikes')
+    last_posted = fields.DateTimeField(name='lastPosted')
+    last_activity = fields.DateTimeField(name='lastActivity')
 
 
 class Comment(object):
 
     def __init__(self, **kwargs):
 
+        self.id = kwargs.get('id', '')
         self.author = kwargs.get('author', '')
         self.content = kwargs.get('content', '')
         self.tags = kwargs.get('tags', [])
@@ -19,6 +39,7 @@ class Comment(object):
 
 class CommentSerializer(Serializer):
     
+    id = fields.StringField()
     author = fields.StringField()
     content = fields.StringField()
     created = fields.DateTimeField()
@@ -35,6 +56,7 @@ class Post(object):
 
     def __init__(self, **kwargs):
 
+        self.id = kwargs.get('id', '')
         self.title = kwargs.get('title', '')
         self.author = kwargs.get('author', '')
         self.content = kwargs.get('content', '')
@@ -48,6 +70,7 @@ class Post(object):
 
 class PostSerializer(Serializer):
     
+    id = fields.StringField()
     title = fields.StringField()
     author = fields.StringField()
     content = fields.StringField()
