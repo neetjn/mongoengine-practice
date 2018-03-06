7157a21cea8e4fb821ada9ecf64a6d58649b5f0b
John Nolette <john@neetgroup.net>
2018-03-06 00:32:45 -0500

    Comments resource

--- blog/resources/comments.py
+++ blog/resources/comments.py
@@ -2,7 +2,16 @@ import falcon
 from blog.utils import to_json, from_json
 
 
-class CommentsResource(object):
+class CommentResource(object):
 
     def on_get(self, req, resp):
+        """Fetch single comment resource."""
+        resp.body = ''
+
+    def on_put(self, req, resp):
+        """Update single comment resource."""
+        resp.body = ''
+
+    def on_delete(self, req, resp):
+        """Delete single comment resource."""
         resp.body = ''
--- blog/resources/posts.py
+++ blog/resources/posts.py
@@ -5,17 +5,17 @@ from blog.utils import to_json, from_json
 class PostResource(object):
 
     def on_get(self, req, resp):
-        """Fetch single post resource"""
+        """Fetch single post resource."""
         resp.status = falcon.HTTP_200
         resp.body = ''
 
     def on_put(self, req, resp):
-        """Update single post resource"""
+        """Update single post resource."""
         resp.status = falcon.HTTP_200
         resp.body = ''
 
     def on_delete(self, req, resp):
-        """Delete single post resource"""
+        """Delete single post resource."""
         resp.status = falcon.HTTP_200
         resp.body = ''
 
@@ -32,6 +32,6 @@ class PostCollectionResource(object):
         resp.body = ''
 
     def on_post(self, req, resp):
-        """Create a new post resource"""
+        """Create a new post resource."""
         resp.status = falcon.HTTP_200
         resp.body = ''
