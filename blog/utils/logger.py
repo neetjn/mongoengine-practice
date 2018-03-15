138ce6ecad4a15e744f54e66433b26bd28d4cb52
John Nolette <john@neetgroup.net>
2018-03-15 00:25:40 -0400

    Finished blog logger

--- blog/utils/logger.py
+++ blog/utils/logger.py
@@ -1,27 +1,77 @@
+import datetime
 import logging
+import sys
+from falcon import Request
 
 
-def construct_log_message(req, msg):
+logging.StreamHandler(sys.stdout)
+
+logger = logging.getLogger()
+logger.addHandler(logging.FileHandler('blog.log'))
+logger.setLevel(logging.DEBUG)
+
+def construct_log_message(req: Request, level: str, msg: str):
     """
+    Constructs log messages using provided request object.
+
+    :param req: Falcon request object to read from.
+    :type req: Request
+    :param level: Logging type to tag in logs.
+    :type level: str
+    :param msg: Message to include in log.
+    :type msg: str
     """
-    message = (f'sender: ["{req.forwarded_host}"]; resource: ["{req.path}"];'
-               f' method: ["{req.method}"]')
+    time = datetime.datetime.utcnow()
+    message = (f'({level}) time: ["{time}"]; sender: ["{req.forwarded_host}"]; resource: ["{req.path}"];'
+               f' method: ["{req.method}"]; message: ["{msg}"];')
     if req.blog_user:
         return f'user: ["{req.blog_user.user_id}", "{req.blog_user.username}"]; ' + message
     return message
 
 
-def debug(req, msg):
+def debug(req: Request, msg: str):
     """
-    """
-    logging.debug(construct_log_message(req, msg))
+    Create debug message for blog logger.
 
-def info(req, msg):
+    :param req: Falcon request object to read from.
+    :type req: Request
+    :param msg: Message to include in log.
+    :type msg: str
     """
-    """
-    logging.debug(construct_log_message(req, msg))
+    logger.debug(construct_log_message(req, 'debug', msg))
 
-def warning(req, msg):
+
+def info(req: Request, msg: str):
     """
+    Create info message for blog logger.
+
+    :param req: Falcon request object to read from.
+    :type req: Request
+    :param msg: Message to include in log.
+    :type msg: str
     """
-    logging.debug(construct_log_message(req, msg))
+    logger.debug(construct_log_message(req, 'info', msg))
+
+
+def warning(req: Request, msg: str):
+    """
+    Create warning message for blog logger.
+
+    :param req: Falcon request object to read from.
+    :type req: Request
+    :param msg: Message to include in log.
+    :type msg: str
+    """
+    logger.debug(construct_log_message(req, 'warning', msg))
+
+
+def critical(req: Request, msg: str):
+    """
+    Create critical message for blog logger.
+
+    :param req: Falcon request object to read from.
+    :type req: Request
+    :param msg: Message to include in log.
+    :type msg: str
+    """
+    logger.debug(construct_log_message(req, 'critical', msg))
