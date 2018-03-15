35bd0c82a8144e17a17c246d190878005208c11b
John Nolette <john@neetgroup.net>
2018-03-15 00:32:47 -0400

    Added basic blog post mod hook

--- blog/hooks/posts.py
+++ blog/hooks/posts.py
@@ -1,1 +1,11 @@
-# TODO: create hook to check post is private, post blongs to user, etc.
+from falcon import Request, Response
+from blog.mediatypes import UserRoles
+
+
+def has_mod_post_access(resource: object, req: Request, resp: Response):
+    """
+    Check if user has access to edit or delete post resource.
+    """
+    if req.blog_user.role == UserRoles.admin or UserRoles.moderator:
+        return True
+    # TODO: complete post mod access hook
