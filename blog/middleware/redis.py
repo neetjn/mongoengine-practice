0ad7cd5a0b479589d5b0df93c3e4b4ef391dd929
John Nolette <jnolette@transparent.com>
2019-02-02 02:26:52 -0500

    Fixed cached bindings

--- blog/middleware/redis.py
+++ blog/middleware/redis.py
@@ -8,14 +8,15 @@ from blog.utils.logger import warning
 client = redis.StrictRedis(host=BLOG_REDIS_HOST, port=BLOG_REDIS_PORT)
 
 
-def cache_key(req, resource) -> str:
+def cache_key(req, resource, uri=None) -> str:
     """Provides unique redis cache key."""
+    uri = uri or req.uri
     if resource.unique_cache:
         user = req.context.get('user')
         if user:
-            return f'{req.uri}+{str(user.id)}'
-        warning(req, f'Could not construct unique key for uri "{req.uri}"')
-    return req.uri
+            return f'{uri}+{str(user.id)}'
+        warning(req, f'Could not construct unique key for uri "{uri}"')
+    return uri
 
 
 class CacheProvider(object):
@@ -28,16 +29,17 @@ class CacheProvider(object):
     def process_response(self, req, resp, resource, req_succeeded):
         """Sets or deletes cache for provided resources."""
         if req_succeeded and resource.use_cache:
+            cache = cache_key(req, resource)
             if req.method == HttpMethods.GET:
-                client.set(cache_key(req, resource), resp.body)
+                client.set(cache, resp.body)
                 if not resp.body:
                     resp.body = resp.cached
             else:
-                client.delete(cache_key(req, resource))
+                client.delete(cache)
                 for resc in resource.cached_resources:
                     tmpl = resc.route.replace('{', '${')  # interpolate for safe formatting
                     # assumes that binded resources ay have routes with similar params
                     route = Template(tmpl).safe_substitute(**req.params)
                     uri = f'{req.protocol}://{req.netloc}{route}'
                     # delete binded cached resources
-                    client.delete(uri)
+                    client.delete(cache_key(req, resc, uri))
